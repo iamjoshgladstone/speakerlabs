a92bcc18eebdb73cbb39c6503026c83e78643a41
Josh Gladstone <joshgladstone1@gmail.com>
2022-03-12 11:04:58 -0700

    setting up routing and a bit of page styling

---
+++ src/pages/Curveballs.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="fit column wrap justify-center items-center content-cetner">
+    <h5>Curveballs</h5>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+    <div class="item">Hello</div>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.item {
+  width: 100px;
+  height: 200px;
+  border: 1px solid yellow;
+}
+</style>
